b57e9c21b9db643eb03049dc722de450a16a6a52
lsh <3040114965@qq.com>
2024-06-05 20:28:00 +0800

    feat: 添加搜索按钮

--- src/components/sys/user/UserManager.tsx
+++ src/components/sys/user/UserManager.tsx
@@ -27,22 +27,26 @@ const UserManager: React.FC = () => {
     pageSize: 5,
     total: 0,
   });
-
+  const [searchText, setSearchText] = useState<string>("");
   useEffect(() => {
-    fetchUsers(pagination.current, pagination.pageSize);
-  }, [pagination.current, pagination.pageSize]);
+    fetchUsers(pagination.current, pagination.pageSize, searchText);
+  }, []);
 
   // 获取用户信息
-  const fetchUsers = async (page: number, pageSize: number) => {
+  const fetchUsers = async (
+    page: number,
+    pageSize: number,
+    searchText: string
+  ) => {
     try {
       const response = await axios.get("/sys-service/user/list", {
-        params: { page: page, pageSize: pageSize },
+        params: { page: page, pageSize: pageSize, nickname: searchText },
       });
       const data = response.data;
       console.log(data);
-      
+
       setUsers(data.records);
-      
+
       setPagination({ ...pagination, total: data.total });
     } catch (error) {
       message.error("获取用户失败");
@@ -74,10 +78,10 @@ const UserManager: React.FC = () => {
     }
   };
 
-  const handleDelete = async (userId: number) => {
+  const handleDelete = async (userId: string) => {
     try {
       await axios.delete(`/sys-service/user/delete/${userId}`);
-      fetchUsers(pagination.current, pagination.pageSize);
+      fetchUsers(pagination.current, pagination.pageSize, searchText);
       message.success("用户删除成功");
     } catch (error) {
       message.error("删除用户失败");
@@ -96,7 +100,7 @@ const UserManager: React.FC = () => {
         await axios.post("/sys-service/user/add", values);
         message.success("添加成功");
       }
-      fetchUsers(pagination.current, pagination.pageSize);
+      fetchUsers(pagination.current, pagination.pageSize, searchText);
       setIsModalVisible(false);
     } catch (error) {
       message.error("操作失败");
@@ -107,11 +111,21 @@ const UserManager: React.FC = () => {
     setIsModalVisible(false);
   };
 
+  const handleSearch = (value: string) => {
+    setSearchText(value);
+    fetchUsers(1, pagination.pageSize, value);
+  };
+
   const columns = [
     { title: "用户名", dataIndex: "username", key: "username" },
     { title: "姓名", dataIndex: "nickName", key: "nickName" },
     { title: "电话", dataIndex: "phone", key: "phone" },
-    { title: "性别", dataIndex: "sex", key: "sex",render: (value)=><>{tsex[value]}</> },
+    {
+      title: "性别",
+      dataIndex: "sex",
+      key: "sex",
+      render: (value) => <>{tsex[value]}</>,
+    },
 
     {
       title: "操作",
@@ -132,6 +146,12 @@ const UserManager: React.FC = () => {
       <Button type="primary" onClick={handleAdd}>
         新增用户
       </Button>
+      <Input.Search
+        placeholder="输入姓名进行搜索"
+        onSearch={handleSearch}
+        style={{ width: 200 }}
+        allowClear
+      />
       <Table
         columns={columns}
         dataSource={users}
